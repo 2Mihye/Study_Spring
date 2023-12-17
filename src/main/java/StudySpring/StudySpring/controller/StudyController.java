8d4c4466537ff7134bfb4dc42baf0e15b9fa274a
Kaye <139112573+2Mihye@users.noreply.github.com>
2023-12-17 22:04:36 +0900

    Spring study

---
+++ src/main/java/StudySpring/StudySpring/controller/StudyController.java
@@ -0,0 +1,14 @@
+package StudySpring.StudySpring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StudyController {
+    @GetMapping("study")
+    public String Study(Model model){
+        model.addAttribute("data", "hello~");
+        return "study";
+    }
+}
---
+++ src/test/java/StudySpring/StudySpring/StudySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package StudySpring.StudySpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
