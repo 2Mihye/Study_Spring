b7d630f44c03e98640019ad94f17fb04f4456d48
2Mihye <leemihye0@gmail.com>
2023-12-23 22:32:24 +0900

    API

--- src/main/java/StudySpring/StudySpring/controller/StudyController.java
+++ src/main/java/StudySpring/StudySpring/controller/StudyController.java
@@ -1,5 +1,6 @@
 package StudySpring.StudySpring.controller;
 
+import org.apache.tomcat.util.net.TLSClientHelloExtractor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,7 +27,7 @@ public class StudyController {
         return "hello " + name; // ResponseBody 때문에 웹에서 소스코드 보기를 해도 현재 코드를 그대로 내려줌.
     }
 
-    @GetMapping("study-spi")
+    @GetMapping("study-Api")
     @ResponseBody
     public Study studyApi(@RequestParam("name") String name) {
         Study study = new Study();
@@ -38,6 +39,32 @@ public class StudyController {
         private String name;
 
         public String getName(){
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    @GetMapping("study-spring")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("study-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
             return name;
         }
 
