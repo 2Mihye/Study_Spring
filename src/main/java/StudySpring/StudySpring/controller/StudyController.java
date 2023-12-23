58aaa68c839e602b46f00677a533a6e1469e8352
2Mihye <leemihye0@gmail.com>
2023-12-23 23:35:38 +0900

    Using Repositories

--- src/main/java/StudySpring/StudySpring/controller/StudyController.java
+++ src/main/java/StudySpring/StudySpring/controller/StudyController.java
@@ -24,47 +24,22 @@ public class StudyController {
     @GetMapping("study-string")
     @ResponseBody // http에서  body 부분에다 직접 넣어주겠다는 의미.
     public String studyString(@RequestParam("name") String name){
-        return "hello " + name; // ResponseBody 때문에 웹에서 소스코드 보기를 해도 현재 코드를 그대로 내려줌.
+        return "hello " + name + "~ :>"; // ResponseBody 때문에 웹에서 소스코드 보기를 해도 현재 코드를 그대로 내려줌.
     }
 
-    @GetMapping("study-Api")
-    @ResponseBody
+    @GetMapping("study-api")
+    @ResponseBody // 이 어노테이션이 붙은채로 객체를 넘기면 HTTP Message Converter이 작동을 하는데 객체라면 JSON으로 바꾸고 문자라면 String으로 바꾼다.
     public Study studyApi(@RequestParam("name") String name) {
         Study study = new Study();
         study.setName(name);
-        return study;
+        return study; // 이로인해 화면에 나오는 코드는 JSON 이라고 한다.
+        // JSON으로 반환하는 것이 기본 default값이다.
     }
 
     static class Study {
         private String name;
 
         public String getName(){
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-    }
-
-    @GetMapping("study-spring")
-    @ResponseBody
-    public String helloString(@RequestParam("name") String name) {
-        return "hello " + name;
-    }
-
-    @GetMapping("study-api")
-    @ResponseBody
-    public Hello helloApi(@RequestParam("name") String name) {
-        Hello hello = new Hello();
-        hello.setName(name);
-        return hello;
-    }
-
-    static class Hello {
-        private String name;
-
-        public String getName() {
             return name;
         }
 
