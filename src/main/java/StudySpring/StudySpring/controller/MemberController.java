44ccde2d0a81f523542e353fbb8fad1b49d5156f
2Mihye <leemihye0@gmail.com>
2023-12-31 23:52:09 +0900

    Spring Study

--- src/main/java/StudySpring/StudySpring/controller/MemberController.java
+++ src/main/java/StudySpring/StudySpring/controller/MemberController.java
@@ -4,7 +4,7 @@ import StudySpring.StudySpring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-@Controller // Controller 어노테이션을 해두면 멤버 컨트롤러 객체를 생성해서 컨테이너에 넣어두고 스프링 빈이 관리된다.
+@Controller // 해당 어노테이션을 해두면 멤버 컨트롤러 객체를 생성해서 컨테이너에 넣어두고 스프링 빈이 관리된다.
 public class MemberController {
     private final MemberService memberService;
     @Autowired
