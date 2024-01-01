3890d99b46fee1926d390c4769483f76e82d11a8
2Mihye <leemihye0@gmail.com>
2024-01-01 23:01:19 +0900

    Spring web Sign-in

--- src/main/java/StudySpring/StudySpring/controller/MemberController.java
+++ src/main/java/StudySpring/StudySpring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package StudySpring.StudySpring.controller;
 
+import StudySpring.StudySpring.domain.Member;
 import StudySpring.StudySpring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller // 해당 어노테이션을 해두면 멤버 컨트롤러 객체를 생성해서 컨테이너에 넣어두고 스프링 빈이 관리된다.
 public class MemberController {
@@ -10,6 +13,21 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "/members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 
 }
