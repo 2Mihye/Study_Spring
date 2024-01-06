af2992ced314dcbffe7d02df7fe2f3131c36725f
2Mihye <leemihye0@gmail.com>
2024-01-06 22:01:07 +0900

    Member List

--- src/main/java/StudySpring/StudySpring/controller/MemberController.java
+++ src/main/java/StudySpring/StudySpring/controller/MemberController.java
@@ -4,8 +4,11 @@ import StudySpring.StudySpring.domain.Member;
 import StudySpring.StudySpring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller // 해당 어노테이션을 해두면 멤버 컨트롤러 객체를 생성해서 컨테이너에 넣어두고 스프링 빈이 관리된다.
 public class MemberController {
@@ -28,6 +31,13 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 
 }
